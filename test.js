fe953cc8d96fce291ab94a47bfc9e4ca61ad5263
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-16 14:15:14 +0200

    meta tweaks

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-'use strict';
-var assert = require('assert');
-var arrayUnion = require('./');
+import test from 'ava';
+import m from './';
 
-it('should union array items', function () {
-	assert.deepEqual(arrayUnion([1, 2, 2, 3, 1, 2, 4], [1, 2, 3, 6, 7]), [1, 2, 3, 4, 6, 7]);
-	assert.deepEqual(arrayUnion(['a', 'a', 'b', 'a'], ['c', 'a', 'd']), ['a', 'b', 'c', 'd']);
+test(t => {
+	t.deepEqual(m([1, 2, 2, 3, 1, 2, 4], [1, 2, 3, 6, 7]), [1, 2, 3, 4, 6, 7]);
+	t.deepEqual(m(['a', 'a', 'b', 'a'], ['c', 'a', 'd']), ['a', 'b', 'c', 'd']);
 });
